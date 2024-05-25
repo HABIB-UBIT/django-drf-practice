25cd3cc67ff1b24743f88885d84fc602f1d8d22a
Muhammad Habib <habibsahabubit@gmail.com>
2024-05-25 18:54:43 +0500

    func based auth

---
+++ 14-funcbasedauth/app.py
@@ -0,0 +1,73 @@
+# import requests
+import json
+
+# URL= "http://127.0.0.1:8000/studentapi/"
+
+# def get_data(id = None):
+#     data= {}
+#     if id is not None:
+#         data= {'id': id}
+#     json_data= json.dumps(data)
+#     r= requests.get(url= URL, data= json_data)
+#     data= r.json()
+#     print(data)
+
+# get_data()
+
+import requests
+
+URL = "http://127.0.0.1:8000/studentapi/"
+
+def get_data(id=None):
+    params = {}
+    if id is not None:
+        params = {'id': id}
+    headers={'content-Type':'application/json'}
+    r = requests.get(url=URL, headers=headers, params=params)
+    if r.status_code == 200:
+        data = r.json()
+        print(data)
+    else:
+        print("Failed to get data. Status code:", r.status_code)
+
+# get_data(1)
+
+def post_data():
+    data={
+        'name': 'Tariq',
+        'roll': 104,
+        'city': 'karachi'
+    }
+    headers={'content-Type':'application/json'}
+    json_data= json.dumps(data)
+    r= requests.post(url= URL, headers=headers, data= json_data)
+    data= r.json()
+    print(data)
+
+# post_data()
+
+def update_data():
+    data={
+        'id': 1,
+        'name': 'Ahmed',
+        'city':'Thatta'
+    }
+    headers={'content-Type':'application/json'}
+    json_data= json.dumps(data)
+    r= requests.put(url= URL,headers=headers, data= json_data)
+    data= r.json()
+    print(data)
+
+# update_data()
+    
+def delete_data():
+    data={
+        'id': 4
+    }
+    json_data= json.dumps(data)
+    headers={'content-Type':'application/json'}
+    r= requests.delete(url= URL, headers=headers, data= json_data)
+    data= r.json()
+    print(data)
+
+delete_data()
---
+++ 14-funcbasedauth/base/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+# Create your views here.
+
+## GET request
+# @api_view()  ## By default it is get
+# def studentapi(request):
+#     return Response({'msg':'Hello World'})
+
+## POST request
+# @api_view(['POST'])
+# def studentapi(request):
+#     if request.method == "POST":
+#         print(request.data)
+#         return Response({'msg':'This is Post request'})
+
+## Combine request
+# @api_view(['Get', 'POST'])
+# def studentapi(request):
+#     if request.method == "GET":
+#         return Response({'msg':'This is Get request'})
+#     if request.method == "POST":
+#         print(request.data)
+#         return Response({'msg':'This is Post request'})
+
+
+## CRUD function
+@api_view(['GET','POST','PUT','DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def studentapi(request, pk= None):
+    if request.method == "GET":
+        # id= request.GET.get('id')
+        # id=pk
+        if pk is not None:
+            stu = Student.objects.get(id=pk)
+            serializer= StudentSerializer(stu)
+            return Response(serializer.data)
+        stu= Student.objects.all()
+        serializer= StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer= StudentSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data Created'})
+        return Response(serializer.errors)
+    
+    if request.method == "PUT":
+        # id= request.data.get('id')
+        stu= Student.objects.get(id = pk)
+        serializer= StudentSerializer(stu, data= request.data ,partial= True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data Updated'})
+        return Response(serializer.errors)
+    
+    if request.method == "DELETE":
+        # id= request.data.get('id')
+        stu= Student.objects.get(id = pk)
+        stu.delete()
+        return Response({'msg':'Data Deleted'})
